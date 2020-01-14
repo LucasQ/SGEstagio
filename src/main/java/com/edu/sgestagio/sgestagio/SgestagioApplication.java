17fd5961532267ad2be67fb630303f4e9edfafa6
LucasQ <mallevo13@gmail.com>
2020-01-14 16:03:29 -0300

    spring structure

---
+++ src/main/java/com/edu/sgestagio/sgestagio/SgestagioApplication.java
@@ -0,0 +1,13 @@
+package com.edu.sgestagio.sgestagio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SgestagioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SgestagioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/edu/sgestagio/sgestagio/SgestagioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edu.sgestagio.sgestagio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SgestagioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
